6a3f208091a2c65125525c90a68fde5d250edb5a
sorenharfagri <58595023+sorenharfagri@users.noreply.github.com>
2020-02-21 17:05:26 +0300

    Refactor

--- client/src/components/infobar/infoBar.js
+++ client/src/components/infobar/infoBar.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './infoBar.css';
 import onlineIcon from '../../icons/onlineIcon.png';
 import closeIcon from '../../icons/closeIcon.png';
-import {Button} from 'react-bootstrap';
 
 import InviteButton from "./InviteButton/InviteButton";
 
 //Данный компонент содержит в себе кнопку с инвайтом в комнату
 
-const InfoBar = ({room, isShowWeb, setShowWeb}) => {
+const InfoBar = ({room}) => {
 
 
 //По нажатию inviteButton появляется всплывающее окно с ссылкой в данный чат
@@ -18,9 +17,6 @@ return (
     <div className="leftInnerContainer">
 <img className="onlineIcon" src={onlineIcon} alt="online"></img>
 <InviteButton room={room}/> 
-<div className="child-button">
-<Button variant="light" >Turn on web</Button>
-</div>
     </div>
     <div className="rightInnerContainer">
 <a href="/"><img src={closeIcon} alt="close"/></a>
