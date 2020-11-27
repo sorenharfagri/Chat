00a213d33e7cef57b103de00b3311eebec278c10
sorenharfagri <58595023+sorenharfagri@users.noreply.github.com>
2020-11-27 20:43:42 +0300

    Comment fix

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -3,12 +3,6 @@ import {userReducer} from "./reducers/userReducer";
 import {loginReducer} from "./reducers/loginReducer";
 import {chatReducer} from "./reducers/chatReducer";
 
-//TODO Сокет создаёт 1 соединение на 1 неймспейс, проверить теорию
-
-// socket.on('roomData', ({users}) => {
-//     console.log(`Got users in root reducer ${users}`)
-// });
-
 export const rootReducer = combineReducers({
     user: userReducer,
     login: loginReducer,
